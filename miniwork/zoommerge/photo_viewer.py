1d7d1f7ea5ea656e73e3411ab2c52b906bf39fea
Le Cong Nguyen <nguyenlc_intern@arrow-tech.vn>
2020-09-16 09:19:55 +0700

    edit

--- miniwork/zoommerge/photo_viewer.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class PhotoViewer(QtWidgets.QGraphicsView):
-
-    photoClicked = QtCore.pyqtSignal(QtCore.QPoint)
-
-    def __init__(self, parent):
-
-        super(PhotoViewer, self).__init__()
